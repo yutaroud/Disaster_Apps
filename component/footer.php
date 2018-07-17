b09e89c48db51bbe0fdbf7253cc36e888a136294
Yuki <yutaroud@yahoo.co.jp>
2018-07-17 18:46:27 +0900

    CSSとBootstrapの追加

---
+++ component/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <small>
+        <a href="https://railstutorial.jp/">安田遠藤浦田研究室</a>
+        by <a href="http://www.michaelhartl.com/">渡辺優樹</a>
+    </small>
+    <nav>
+        <ul>
+            <li>"test"</li>
+
+        </ul>
+    </nav>
+</footer>
